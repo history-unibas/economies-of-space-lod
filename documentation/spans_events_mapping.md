2d97599d7e02a4ce52d8a37a65c541a01fe0248e
Francesco Beretta <francesco.beretta@cnrs.fr>
2026-05-28 19:12:35 +0200

    refactoring of t_spans and mapping documentation

--- documentation/spans_events_mapping.md
+++ documentation/spans_events_mapping.md
@@ -1,3 +1,18 @@
 
 
 
+## Spans
+
+* Spans are collections of one or more tokens with some meaning and refer to named entities or values
+  * spans also have some grammatical or NLP attributes like 'reference', 'head', 'apposition', etc.
+  * they refer to objects of type 'person', 'organisation', etc.
+* They have been extracted to the table *t_spans*
+* We map them to instances of the class *sdh-info:C16 Chunk*
+* The URI of spans/chnks is provided by a concatenation of the 'entityid' table column and the span 'id' attribute in the form: *0eb11e76-b4de-4462-b15d-5a443d5b4523_20250307_span0*
+  * this columns 'span_uri' is the primary key of the table
+
+
+### Mapping of attributes
+
+* @text is mapped to the primitive value  crm:E62 String associated with the property crm:P190 has symbolic content
+
--- sql_queries_views/db_create_tables_spans.sql
+++ sql_queries_views/db_create_tables_spans.sql
@@ -5,15 +5,21 @@
  * 
  */
 
+
+-- inspect project entry
 select *
 from project_entry pe
 where pe.annotation_automated is not null
 limit 10;
 
 
+/*
+ * About the different kinds of spans: reference, head, etc.
+ * cf. Annotation manual 2.2.1 : Reference
+ * 
+ */
 
-
--- first level reference span
+-- first level span
 select unnest(xpath('//spans/span', 
 pe.annotation_automated)) as span,
 year, dossierid, 
@@ -43,12 +49,15 @@ select unnest(xpath('//span',
 pe.annotation_automated)) as span,
 year, dossierid, 
 --pageid, 
+-- the entryid is the main identifier of a text
 entryid
 from project_entry pe 
 where pe.dossierid = 'HGB_1_002_026' 
 AND pe.annotation_automated is not null
 )
 select dossierid, entryid, year, span, 
+-- there are strings in the @id, 
+-- no cast to integer possible here
 	(xpath('/span/@id', 
 	span))[1]::text as span_id
 from tw1
@@ -69,6 +78,8 @@ where entryid='bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
 AND pe.annotation_automated is not null
 )
 select ROW_NUMBER() OVER ()::BIGINT AS pk_t_spans,
+concat(entryid, '_span', (xpath('/span/@id', 
+	span))[1]::text) as span_uri, 
 year, 
 	(xpath('/span/@text', 
 	span))[1]::text as span_text,
@@ -80,6 +91,10 @@ year,
 	span))[1]::text as span_element,
 	(xpath('/span/@id', 
 	span))[1]::text as span_id,
+	(xpath('/span/@start', 
+	span))[1]::text as token_start,
+	(xpath('/span/@end', 
+	span))[1]::text as token_end,
 	xpath('/span/span/@id', 
 	span) as children,
 	span, null as parent_span_id, dossierid, entryid
@@ -88,7 +103,7 @@ order by span_id;
 
 
 -- create table with all the spans
-drop table t_spans	;
+drop table t_spans CASCADE	;
 create table t_spans as
 with tw1 as (
 select unnest(xpath('//span', 
@@ -100,7 +115,9 @@ from project_entry pe
 where pe.annotation_automated is not null
 )
 select 
- 	ROW_NUMBER() OVER ()::BIGINT AS pk_t_spans,
+ 	--ROW_NUMBER() OVER ()::BIGINT AS pk_t_spans,
+ 	concat(entryid, '_span', (xpath('/span/@id', 
+	span))[1]::text) as span_uri, 
  	year, 
 	(xpath('/span/@text', 
 	span))[1]::text as span_text,
@@ -112,21 +129,25 @@ select
 	span))[1]::text as span_element,
 	(xpath('/span/@id', 
 	span))[1]::text as span_id,
+	(xpath('/span/@start', 
+	span))[1]::text as token_start,
+	(xpath('/span/@end', 
+	span))[1]::text as token_end,
 	xpath('/span/span/@id', 
-	span) as children,
-	span, 
-	0 as fk_parent_span,
-	'' as parent_span_id, dossierid, entryid, pageid
+	span) as children, '' as parent_span_id,
+	span, dossierid, entryid, pageid
 from tw1
 order by dossierid, year, entryid;
 
-alter table t_spans add primary key (pk_t_spans);
+
+-- verify index types !!!
+ALTER table t_spans add primary key (span_uri);
 CREATE INDEX t_spans_class_index ON t_spans (span_class);
 CREATE INDEX t_spans_norm_index ON t_spans (span_norm);
 CREATE INDEX t_spans_span_text_idx ON t_spans (span_text);
 CREATE INDEX t_spans_entryid_idx ON t_spans (entryid);
 
--- One can add towho indexes on table
+-- One can add two indexes on table
 --CREATE INDEX USING GIN for trigrams
 
 
@@ -135,14 +156,16 @@ vacuum public.t_spans;
 
 select * 
 from t_spans
-where entryid = 'bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
+--where entryid = 'bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
+where entryid = '1a788909-9885-4bf2-9352-ac73fffabe4b_20250307'
 order by span_id;
 limit 200;
 
 
-
+-- 28 May 2028: 2'569'255
 select count(*) as number
 from t_spans;
+
 
 
 select *
@@ -151,13 +174,16 @@ order by dossierid, year, span_id
 offset 200
 limit 50;
 
--- empty column !!!
---update t_spans ts
-set parent_span_id = null;
+select *
+from t_spans ts 
+limit 10;
+
+
+alter table t_spans add column parent_span_uri text;
 
 
 -- children with parents
-select pk_t_spans as fk_t_spans, span, unnest(children)::text::integer as child_id, span_id as parent_id, ts.dossierid, ts.entryid 
+select span_uri as parent_span_uri, span, unnest(children)::text::integer as child_id, span_id as parent_id, ts.dossierid, ts.entryid 
 from t_spans ts 
 where ts.entryid = 'cc9e5200-8464-4de6-bca8-a65ea29ea72d_20250307'
 order by parent_id, child_id
@@ -171,37 +197,48 @@ limit 20;
 -- and make joins more easy
 with tw1 as (
 select unnest(children)::text as child_id,
-pk_t_spans, span_id as parent_id, ts.dossierid, ts.entryid
-from t_spans ts)
+span_uri as parent_span_uri, span_id as parent_id, ts.entryid
+from t_spans ts
+)
 update t_spans ts
-set fk_parent_span = tw1.pk_t_spans
-from tw1
+set parent_span_uri = tw1.parent_span_uri, 
+   parent_span_id = tw1.parent_id
+   from tw1
 where ts.span_id = tw1.child_id 
-and ts.dossierid = tw1.dossierid 
+--and ts.dossierid = tw1.dossierid 
 and  ts.entryid = tw1.entryid;
 
 
-CREATE INDEX idx_t_spans_fk_parent_span ON t_spans (fk_parent_span);
+CREATE INDEX idx_t_spans_fk_parent_uri ON t_spans (parent_span_uri);
 
 
 select * from 
 t_spans ts 
-where ts.fk_parent_span = 0
+where ts.parent_span_uri is null
 limit 10;
 
 -- the fk_parent_span column must be NULL if no reference (and not 0) 
 -- in order to add this contraint because many spans do not have a parent span
-update t_spans ts set fk_parent_span = null
-where ts.fk_parent_span = 0;
 
-ALTER TABLE t_spans ADD CONSTRAINT fk_t_spans_parent_span 
-	FOREIGN KEY (fk_parent_span) REFERENCES t_spans(pk_t_spans);
+ALTER TABLE t_spans ADD CONSTRAINT fk_t_spans_parent_span_uri
+	FOREIGN KEY (parent_span_uri) REFERENCES t_spans(span_uri);
 
 
 
 select *
 from t_spans ts 
-where ts.entryid='bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
+--where ts.entryid='bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
+where ts.entryid='1a788909-9885-4bf2-9352-ac73fffabe4b_20250307'
+--where ts.entryid = 'cc9e5200-8464-4de6-bca8-a65ea29ea72d_20250307'
+--order by parent_id, child_id
+limit 200;
+
+-- verify parents
+select ts.span_id, ts.span, ts.parent_span_id, parent_span.span_id, parent_span.span
+from t_spans ts left join t_spans as parent_span
+on ts.parent_span_uri = parent_span.span_uri
+--where ts.entryid='bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
+where ts.entryid='1a788909-9885-4bf2-9352-ac73fffabe4b_20250307'
 --where ts.entryid = 'cc9e5200-8464-4de6-bca8-a65ea29ea72d_20250307'
 --order by parent_id, child_id
 limit 200;
@@ -213,6 +250,18 @@ limit 200;
 
 select count(*)
 from t_spans ts ;
+
+-- 28 May 2026 : 694961
+select count(*)
+from t_spans ts 
+where ts.span_element = 'reference';
+
+
+select ts.span_element, count(*) number
+from t_spans ts 
+group by span_element 
+order by number desc;
+
 
 
 select ts.span_class, count(*) number
@@ -234,6 +283,114 @@ from t_spans ts
 	join geo_address ga on ga.signatur = sd.stabsid 
 where ts.span_class = 'buyer'
 order by span_text ;
+
+
+/*
+ * Spans to tokens
+*/
+
+select * 
+from t_spans
+--where entryid = 'bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
+where entryid = '1a788909-9885-4bf2-9352-ac73fffabe4b_20250307'
+order by span_id;
+limit 200;
+
+
+
+
+-- tokens
+select (unnest(xpath('//text/token/text()', 
+pe.annotation_automated)))::text as token,
+(unnest(xpath('//text/token/@token_id', 
+pe.annotation_automated)))::text::integer as token_id,
+year, dossierid, 
+--pageid, 
+entryid
+from project_entry pe 
+where pe.dossierid = 'HGB_1_002_026' 
+AND pe.annotation_automated is not null
+order by year
+
+
+with tw1 as (
+-- tokens
+select (unnest(xpath('//text/token/text()', 
+pe.annotation_automated)))::text as token,
+(unnest(xpath('//text/token/@token_id', 
+pe.annotation_automated)))::text::integer as token_id,
+--year, dossierid, 
+--pageid, 
+entryid
+from project_entry pe 
+--where pe.dossierid = 'HGB_1_002_026' 
+where entryid = '1a788909-9885-4bf2-9352-ac73fffabe4b_20250307'
+AND pe.annotation_automated is not null
+)
+select concat(entryid, '_tok', token_id::text) token_uri, token token_text, token_id, entryid
+from tw1;
+
+
+
+
+create table t_token as
+with tw1 as (
+select (unnest(xpath('//text/token/text()', 
+pe.annotation_automated)))::text as token,
+(unnest(xpath('//text/token/@token_id', 
+pe.annotation_automated)))::text::integer as token_id,
+entryid
+from project_entry pe 
+where pe.annotation_automated is not null
+)
+select concat(entryid, '_tok', token_id::text) token_uri, token token_text, token_id, entryid
+from tw1;
+
+
+
+ALTER table t_token add primary key (token_uri);
+CREATE INDEX t_token_text_idx ON t_token(token_text);
+CREATE INDEX t_token_entryid_idx ON t_token (entryid);
+
+analyze public.t_token;
+vacuum public.t_token;
+
+
+-- explore
+select *
+from t_token
+limit 10;
+
+-- 28 May 2026: 4'984'355
+select count(*)
+from t_token;
+
+
+
+
+
+
+-- generate link token -> spans
+with tw1 as (
+SELECT 
+	span_id,
+    generate_series(token_start::integer, token_end::integer) AS target,
+    --token_start, token_end, 
+    entryid 
+FROM t_spans
+--where entryid = 'bfe0d71e-df8d-448d-89ad-f208e760865e_20250307'
+where entryid = '1a788909-9885-4bf2-9352-ac73fffabe4b_20250307'
+)
+select concat(entryid, '_span', span_id) as span_uri, 
+concat(entryid, '_tok', target) as token_uri
+from tw1;
+
+    
+    
+
+
+
+
 
 
 
